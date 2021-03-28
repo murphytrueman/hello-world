22324a4436b43953571a008917158b5e80344b0f
Murphy Trueman <hello@murphytrueman.com>
2021-03-28 22:02:27 +1100

    Added files for part two

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,5 @@
 import React from "react"
+
 export default function Home() {
-  return (
-    <div style={{ color: `purple` }}>
-      <h1>Hello Gatsby!</h1>
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
-  )
+  return <div>Hello world!</div>
 }

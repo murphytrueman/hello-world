79f55f114a20374e6ca02ead837775ce5c4b73b5
Murphy Trueman <hello@murphytrueman.com>
2021-03-29 09:00:35 +1100

    Compressed stg 1, stg 2 of tutorial. Added header and footer components

---
+++ src/components/layout.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { Link } from "gatsby"
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+export default function Layout({ children }) {
+  return (
+    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <header style={{ marginBottom: `1.5rem` }}>
+        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+          <h3 style={{ display: `inline` }}>Murphy Trueman</h3>
+        </Link>
+        <ul style={{ listStyle: `none`, float: `right` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
+      </header>
+      {children}
+
+      <footer
+        style={{
+          backgroundColor: `rgba(0,0,0,0.05)`,
+          padding: `10px`,
+          fontSize: `14px`,
+        }}
+      >
+        Test footer content
+      </footer>
+    </div>
+  )
+}
